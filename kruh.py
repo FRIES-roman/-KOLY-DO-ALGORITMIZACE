18d729506894b1a4e86bd5b6af06fe8e546d76eb
FRIES-roman <24302@student.vitkovickastredni.cz>
2025-02-14 08:28:23 +0100

    kruh.py

--- kruh.py
+++ kruh.py
@@ -1,28 +1,11 @@
 import tkinter as tk
 
-
 root = tk.Tk()
-root.title("Mezikružné kružnice")
+root.title("Kreslení kruhu")
 
-
-canvas = tk.Canvas(root, width=500, height=500, bg="white")
+canvas = tk.Canvas(root, width=500, height=500, bg='white')
 canvas.pack()
 
-
-x1, y1 = 50, 50  
-x2, y2 = 450, 450  
-step = 20  
-
-for i in range(10): 
-    if i % 2 == 0:  
-        color = "red"
-    else:  
-        color = "blue"
-    
-    canvas.create_oval(x1, y1, x2, y2, outline=color, width=3)
-    x1 += step  
-    y1 += step
-    x2 -= step  
-    y2 -= step
+canvas.create_oval(100, 100, 300, 300, outline='red', width=2)
 
 root.mainloop()
